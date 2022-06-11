effa14bf75374f50fd0da276bdb7e86030233f8e
Jaoe <julianorejuela@hotmail.com>
2022-06-10 20:16:08 -0500

    add allowed hosts

--- appweb_comodin/settings.py
+++ appweb_comodin/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'django-insecure-v9!#of8u&8q8glq=dwq19k)^p4e52b3g$qihm60zc^5j*%9=3-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://jokerevents-api.herokuapp.com/api/users/auth'
+]
 
 
 # Application definition
