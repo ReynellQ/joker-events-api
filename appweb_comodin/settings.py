7e6db65c6e674356f11fb50402f537100b366cea
Reynell Quevedo <reynell200501@gmail.com>
2022-06-15 09:19:52 -0500

    Set DEBUG as False

--- appweb_comodin/settings.py
+++ appweb_comodin/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-v9!#of8u&8q8glq=dwq19k)^p4e52b3g$qihm60zc^5j*%9=3-
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
