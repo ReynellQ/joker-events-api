6346d6ae66ee58e36fba4d92899a4cb85b20ec63
Reynell Quevedo <reynell4587@hotmail.com>
2022-06-23 17:43:57 -0500

    Add serializers to events

--- events/models.py
+++ events/models.py
@@ -1,16 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Events(models.Model):
-    name = models.CharField(max_length=50)
-    place = models.CharField(max_length=100)
-    capacity = models.IntegerField()
+    title = models.CharField(max_length=50)
+    lugar = models.CharField(max_length=100)
+    aforo = models.IntegerField()
     description = models.TextField()
     cost = models.DecimalField(default=0.0, max_digits=14, decimal_places=2)
     date = models.DateField()
-    active = models.BooleanField()
-    long = models.DecimalField(max_digits=8, decimal_places=3)
-    lat = models.DecimalField(max_digits=8, decimal_places=3)
+    visible = models.BooleanField()
+    image = models.CharField(max_length=200, blank=True)
+    fechaInicio = models.DateField()
+    fechaFin = models.DateField()
+    gmaps = models.CharField(max_length=100)
+    disponible = models.IntegerField()
+    contacto = models.CharField(max_length = 100, default="")
+    createdBy = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
+    createdAt = models.DateField(),
+    visible = models.BooleanField(default=True)
     class Meta():
         db_table = 'events'
         order_with_respect_to = 'date'
--- events/serializer.py
+++ events/serializer.py
@@ -3,16 +3,24 @@ from rest_framework import serializers
 
 
 class EventSerializer(serializers):
-    name = serializers.CharField(max_length=50)
-    place = serializers.CharField(max_length=100)
-    capacity = serializers.IntegerField()
+    title = serializers.CharField(max_length=50)
+    lugar = serializers.CharField(max_length=100)
+    image = serializers.CharField(required=False, max_length=200)
+    aforo = serializers.IntegerField()
     description = serializers.CharField()
-    cost = serializers.DecimalField(
+    precioBoleta = serializers.DecimalField(
         default=0.0, max_digits=14, decimal_places=2)
-    date = serializers.DateField()
-    active = serializers.BooleanField()
-    long = serializers.DecimalField(max_digits=8, decimal_places=3)
-    lat = serializers.DecimalField(max_digits=8, decimal_places=3)
+    fechaInicio = serializers.DateField()
+    fechaFin = serializers.DateField()
+    gmaps = serializers.CharField(max_length=100)
+    disponible = serializers.IntegerField()
+    contacto = serializers.CharField(max_length = 100)
+    resources: serializers.ListField(
+        child=serializers.CharField(max_length=200)
+    )
+    createdBy = serializers.CharField( max_length=200)
+    createdAt = serializers.DateField()
+    visible = serializers.BooleanField()
 
 
 class EventIDSerializer(EventSerializer):
