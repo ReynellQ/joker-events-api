b90c2e12fe63bfbffc949bed355d8c2294f1bb74
Reynell Quevedo <reynell4587@hotmail.com>
2022-07-11 12:54:03 -0500

    Working on POST /api/participant

--- participants/serializer.py
+++ participants/serializer.py
@@ -4,8 +4,9 @@ from rest_framework import serializers
 from django.utils.translation import gettext as _
 from noticias.models import News
 from django.contrib.auth.models import User
+from participants.models import Participant
 from users.models import Profile, Rol
-
+from events.models import Events
 
 class ParticipantSerializer(serializers.Serializer):
     """
@@ -13,12 +14,93 @@ class ParticipantSerializer(serializers.Serializer):
     """
     cedula =  serializers.CharField()
     nombre = serializers.CharField()
-    apellidos = serializers.CharField()
+    apellido = serializers.CharField()
     email = serializers.EmailField()
-    celular = serializers.CharField()
+    ciudad = serializers.CharField()
+    telefono = serializers.CharField()
+    address = serializers.CharField()
     birthday = serializers.DateField()
     password = serializers.CharField()
 
+    def getParticipant(self):
+        p = None
+        try:
+            p = Participant.objects.get(cedula = self.data["cedula"])
+            self.instance = p
+        except Exception as e:
+            print(repr(e))
+        return p
+
+    
     def create(self, data):
-        u: User = User.objects.get(username=data.pop('createdBy'))
-        return News.objects.create(createdBy=u, **data)
+        dataUser ={
+            "username":data["email"], "first_name":data["nombre"], "last_name" : data["apellido"],
+            "is_active":True, "email" :data["email"], "password" : data["password"]
+        } 
+        u = User.objects.create_user(**dataUser)
+        dataProfile = {
+            "rol": "participante", "address":data["address"], "ciudad":data["ciudad"],
+            "telefono": data["telefono"]
+        }
+        for attr, value in dataProfile.items():
+            setattr(u.profile, attr, value)
+        u.save()
+        p = Participant(profile = u.profile, cedula = data["cedula"], birthdate = data["birthday"])
+        p.save()
+        
+        return p
+
+class PaymentSerializer(serializers.Serializer):
+    nameOnCard = serializers.CharField()
+    cardNumber = serializers.CharField()
+    expiryDate = serializers.DateField()
+    cvv = serializers.CharField()
+
+class InscriptionSerializer(serializers.Serializer):
+    participante = ParticipantSerializer()
+    evento = serializers.IntegerField()
+    payment = PaymentSerializer()
+    actividades = serializers.ListField(
+        child=serializers.IntegerField()
+    )
+
+    def validate_participante(self, value):
+        p = ParticipantSerializer(data = value)
+        if not p.is_valid():
+            raise serializers.ValidationError(p.errors)
+        if p.getParticipant() == None:
+            p = ParticipantSerializer(data = value)
+            p.is_valid()
+            p.save()
+        return p
+    
+    def validate_evento(self, value):
+        try:
+            Events.objects.get(id = value)
+        except:
+            raise serializers.ValidationError("The event doesn't exists.")
+        return int(value)
+
+    def validate_payment(self, value):
+        from random import randint
+        p = PaymentSerializer(data = value)
+        if p.is_valid():
+            r = randint(0, 10)
+            print(r)
+            if r <= 8:
+                return p
+            else:
+                raise Exception("Ocurrio un error validando sus datos de pago")
+        else:
+            return serializers.ValidationError(p.errors)
+
+    def process(self):
+        event : Events= Events.objects.get(id=self.validated_data["evento"])
+        event.disponible-=1
+        event.save()
+        if event.disponible < 0:
+            raise Exception("El evento no tiene aforo")
+        pass
+
+
+
--- participants/views.py
+++ participants/views.py
@@ -1,3 +1,51 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views import View
+from django.db import transaction
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
+
+import json
+
+from events.serializer import EventIDSerializer
+from events.models import Events
+
+from participants.serializer import InscriptionSerializer, ParticipantSerializer
 
 # Create your views here.
+
+@method_decorator(csrf_exempt, name='dispatch')
+class ParticipantView(View):
+    def post(self,request):
+        try:
+            requestData: dict = json.loads(request.body.decode('utf-8'))
+            serializer = InscriptionSerializer(data = requestData)
+            if serializer.is_valid():
+                with transaction.atomic():
+                    serializer.process()
+                    raise Exception("ONLY FOR ROLLBACK PURPOSES")
+            else:
+                raise Exception(serializer.errors)
+            # participant = ParticipantSerializer(data = requestData["participante"])
+            
+            # with transaction.atomic():
+            #     if not participant.is_valid():
+            #         raise Exception(participant.errors)
+            #     if participant.getParticipant() != None:
+            #         p = participant.getParticipant()
+            #     else:
+            #         p = participant.save()
+                    
+            #     event : Events= Events.objects.get(id=requestData["evento"])
+            #     event.disponible-=1
+            #     event.save()
+            #     if event.description < 0:
+            #         raise Exception("El evento no tiene aforo")
+                
+            return JsonResponse({"status" : True})
+        except Exception as e:
+            print(repr(e))
+            print(e.args)
+            return JsonResponse({"status" : False})
+        
