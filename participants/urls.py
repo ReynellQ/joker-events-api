aa571cec4a2029852cbb43bd36fcac88fc9b6ff3
Reynell Quevedo <reynell4587@hotmail.com>
2022-07-15 18:13:20 -0500

    change to urls

--- participants/urls.py
+++ participants/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ParticipantView.as_view(), name="participant_view"),
-    path("<int:event_id>/", views.EventInscriptionView.as_view(), name="event_inscription_view"),
+    path("participants", views.ParticipantView.as_view(), name="participant_view"),
+    path("participants/<int:event_id>/", views.EventInscriptionView.as_view(), name="event_inscription_view"),
+    path("devoluciones", views.DevolutionView.as_view(), name="event_inscription_view"),
 ]
