85135213a07b2d70834a63c19725417361d3254c
Reynell Quevedo <reynell4587@hotmail.com>
2022-07-18 17:50:17 -0500

    fix

--- events/serializer.py
+++ events/serializer.py
@@ -59,6 +59,7 @@ class EventSerializer(serializers.Serializer):
                 'createdBy':instance.createdBy.email,
                 'resources': list(map(lambda r : r.media, MediaEvents.objects.filter(id_event= instance.id)))
             }
+        ret.pop('id')
         return ret
 
 
