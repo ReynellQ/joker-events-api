4e10a2bf40f0539c2f56f41af9b7abec18917d72
Reynell Quevedo <reynell4587@hotmail.com>
2022-08-03 23:23:06 -0500

    activities by event_id

--- activities/urls.py
+++ activities/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.ActivitiesView.as_view(), name="activities_view"),
+    path("<int:event_id>", views.EventActivitiesView.as_view(), name="event_activities_view"),
 ]
